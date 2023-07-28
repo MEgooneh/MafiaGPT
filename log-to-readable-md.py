bd0b7894618d57381209bd1a3f6ab4df6c82342f
MEgooneh <mohammad.erfan.gooneh@gmail.com>
2023-07-28 22:05:21 +0330

    adding a program to make log readable

---
+++ log-to-readable-md.py
@@ -0,0 +1,116 @@
+"""
+    This is for making the log.json readable as an article in MarkDown format.
+    You should pass the log.json file to the program while excuting it.
+"""
+import json, sys
+
+file = sys.argv[1]
+log = json.load(open(file , 'r'))
+
+roles = {}
+
+text = ""
+
+def results(votes) :
+    s = ""
+    for i in range(7) : 
+        if votes[i] > 0 :
+            s += f"``` Player {i} : {'+' * votes[i]} ```"
+            s += '<br>'
+    return s
+
+def new_lines_quote(s) : 
+    return s.replace('\n' , "<br> ")
+
+for i in log : 
+    event = i['event']
+    if 'content' in i.keys() : 
+        content = i['content']
+    if event == 'roles' : 
+        roles[content['player']] = content['role']
+        text += f"""
+Player *{content['player']}* is *{content['role']}*
+"""
+    elif event == 'cycle' : 
+        text += f"""
+# It's **{content}** !
+"""
+    elif event == 'speech' : 
+        s = new_lines_quote(content['context'])
+        text +=  f"""
+**Player {content['player']}** ({roles[content['player']]}) : 
+
+> {s}
+"""
+    elif event == 'vote_start' : 
+        text += f"""
+# Voting starts
+"""
+    elif event == 'vote_end' : 
+        text += f"""
+# Voting ends
+"""
+    elif event == 'vote_results' : 
+        text += f"""
+{results(content)}
+"""
+    elif event == 'voted' : 
+        text += f"""
+Player *{content['player']}* ({roles[content['player']]}) Voted to Player *{content['voted_to_player']}* ({roles[content['voted_to_player']]}).
+
+Reason : 
+
+> {new_lines_quote(content['reason'])}
+"""
+    elif event == 'healed' : 
+        text += f"""
+Medic decided to heal Player *{content['player']}* ({roles[content['player']]})
+
+Reason : 
+
+> {new_lines_quote(content['reason'])}
+"""
+    elif event == 'targeted' : 
+        text += f"""
+Werewolves decided to kill Player *{content['player']}* ({roles[content['player']]})
+
+Reason : 
+
+> {new_lines_quote(content['reason'])}
+"""
+    elif event == 'killed' : 
+       text += f"""
+*{content['player']}* ({roles[content['player']]}) is DEAD!
+"""
+    elif event == 'inquiried' : 
+        text += f"""
+Seer decided to inquiry Player *{content['player']}* ({roles[content['player']]})
+
+Reason : 
+
+> {new_lines_quote(content['reason'])}
+
+Answer : 
+
+{content['context']}
+"""
+    elif event == 'notetaking' : 
+        s = new_lines_quote(content['context'])
+        text +=  f"""
+**Player {content['player']}** ({roles[content['player']]}) is taking note for himself : 
+
+> {s}
+"""
+    elif event == 'end' : 
+        text += f"""
+# Game is over! and the winner is **{content['winner']}**
+"""
+    text += """
+---
+"""
+
+
+# Saving the file
+with open('new.md', 'w') as file : 
+    file.write(text)
+print("Saved as new.md!")
